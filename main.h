8722dd90d103b586161f7520132c493e2f63ffe3
Noah Chernet <noahchernet@yahoo.com>
2021-10-03 18:43:58 +0300

    Added prototypes to main.h

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef _PRINTF_MAIN_H
+#define _PRINTF_MAIN_H
+
+#include "stdarg.h"
+#include "stdlib.h"
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif /* _PRINTF_MAIN_H */
